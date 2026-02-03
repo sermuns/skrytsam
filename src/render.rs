84a7921cce365fccf806c507d2aac9f08572c8da
Samuel Åkesson <samak519@student.liu.se>
2026-02-03 21:14:48 +0100

    begin trying to add embed packages locally

--- src/render.rs
+++ src/render.rs
@@ -1,4 +1,5 @@
-use color_eyre::eyre::{ContextCompat, WrapErr, bail};
+use color_eyre::eyre::{ContextCompat, bail};
+use rust_embed::Embed;
 use typst_as_lib::{TypstAsLibError, TypstEngine};
 use typst_library::{
     diag::Warned,
@@ -7,45 +8,52 @@ use typst_library::{
 };
 
 const NOTO_SANS_REGULAR: &[u8] = include_bytes!("../assets/NotoSans-Regular.ttf");
-// const NOTO_SANS_ITALIC: &[u8] = include_bytes!("../assets/NotoSans-Italic.ttf");
 const NOTO_SANS_BOLD: &[u8] = include_bytes!("../assets/NotoSans-Bold.ttf");
-// const NOTO_SANS_BOLD_ITALIC: &[u8] = include_bytes!("../assets/NotoSans-BoldItalic.ttf");
 const MONASPACE_KRYPTON: &[u8] = include_bytes!("../assets/MonaspaceKrypton-Regular.otf");
 const MONASPACE_KRYPTON_BOLD: &[u8] = include_bytes!("../assets/MonaspaceKrypton-Bold.otf");
 
-pub fn compile_svg(template_str: &str, input: Dict) -> color_eyre::Result<String> {
-    let languages_template = TypstEngine::builder()
-        .main_file(template_str)
+const LANGUAGES_TEMPLATE: &str = include_str!("../typst/languages.typ");
+const COMMON_LIB: &str = include_str!("../typst/lib.typ");
+
+#[derive(Embed)]
+#[folder = "typst/packages/"]
+#[include = "*.typ"]
+#[exclude = "**/tests/*"]
+#[exclude = "**/gallery/*"]
+struct TypstPackage;
+
+impl TypstPackage {
+    fn iter_path_contents()
+}
+
+pub fn compile_svg(input: Dict) -> color_eyre::Result<String> {
+    let engine = TypstEngine::builder()
+        .with_static_source_file_resolver([
+            ("languages.typ", LANGUAGES_TEMPLATE),
+            ("lib.typ", COMMON_LIB),
+        ].iter().chain(TypstPackage::iter_path_contents()))
         .fonts([
             NOTO_SANS_REGULAR,
-            // NOTO_SANS_ITALIC,
             NOTO_SANS_BOLD,
-            // NOTO_SANS_BOLD_ITALIC,
             MONASPACE_KRYPTON,
             MONASPACE_KRYPTON_BOLD,
         ])
-        .with_package_file_resolver()
         .build();
 
     let warned_document: Warned<Result<PagedDocument, TypstAsLibError>> =
-        languages_template.compile_with_input(input);
+        engine.compile_with_input("languages.typ", input);
 
     let warnings = warned_document.warnings;
     if !warnings.is_empty() {
-        bail!(
-            "Typst had warnings: {}",
-            warnings
-                .iter()
-                .enumerate()
-                .fold(String::new(), |acc, (i, warning)| acc
-                    + &format!("\n {}: {}", i + 1, warning.message))
-        );
+        bail!("typst had warnings: {:#?}", warnings);
     }
-    let document_pages: Vec<Page> = warned_document.output.wrap_err("ERROR COMPILING")?.pages;
-    if document_pages.len() > 1 {
-        println!("output document has multiple pages!")
+
+    let output_pages: Vec<Page> = warned_document.output?.pages;
+    if output_pages.len() > 1 {
+        bail!("output document has multiple pages!")
     }
-    let first_and_only_page = document_pages
+
+    let first_and_only_page = output_pages
         .first()
         .wrap_err("output document has no pages!")?;
 
